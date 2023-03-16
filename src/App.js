24d8b01321621a32fc294ef3ce4f5c207992c7f0
titoworlddev <titocristian.dev@gmail.com>
2023-03-16 23:18:45 +0100

    Empezando el proyecto

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      {/* TODO: Antes de nada estudiar metodologia BEM */}
       <h1>Formulario de practica</h1>
     </div>
   );
