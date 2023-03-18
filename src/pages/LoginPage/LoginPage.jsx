41645aebaeaca77be49e45e1610f2e67cf596423
titoworlddev <titocristian.dev@gmail.com>
2023-03-18 12:54:04 +0100

    Cambios en la estrucutra de LoginPage

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,9 +1,15 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import './LoginPage.css';
+
+import { MdEmail } from 'react-icons/md';
+import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
+
 import SocialButtons from '../../components/SocialButtons/SocialButtons';
 
 export default function LoginPage() {
+  const [showPassword, setShowPassword] = React.useState(false);
+
   return (
     <main className="main main--login">
       <section className="texts">
@@ -11,27 +17,36 @@ export default function LoginPage() {
         <h1 className="texts__title">Entrar</h1>
         <p className="texts__subtitle">
           ¿No tienes una cuenta?
-          <a href="#">Registrarse</a>
+          <a href="">Registrarse</a>
         </p>
       </section>
 
       <form className="form">
-        <input
-          className="input"
-          type="email"
-          id="form__email"
-          placeholder="Email"
-        />
-        <input
-          className="input"
-          type="password"
-          id="form__password"
-          placeholder="Contraseña"
-        />
+        <div className="input-group">
+          <input required="required" type="text" id="form__email" />
+          <label>Email</label>
+          <div className="input__icon">
+            <MdEmail />
+          </div>
+        </div>
+
+        <div className="input-group">
+          <input
+            required="required"
+            type={showPassword ? 'text' : 'password'}
+            id="form__password"
+          />
+          <label>Contraseña</label>
+          <div
+            className="input__icon input__icon--password"
+            onClick={() => setShowPassword(!showPassword)}>
+            {showPassword ? <AiFillEyeInvisible /> : <AiFillEye />}
+          </div>
+        </div>
 
         <button className="button">Entrar</button>
 
-        <a href="#">¿Has olvidado la contraseña?</a>
+        <a href="">¿Has olvidado la contraseña?</a>
       </form>
 
       <SocialButtons />
