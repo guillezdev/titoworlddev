4cdc7f1b32821338fc94412f94143dae793f8583
titoworlddev <titocristian.dev@gmail.com>
2023-03-18 21:56:31 +0100

    Arreglado el responsive

--- src/pages/ClientRegistryPage/ClientRegistryPage.jsx
+++ src/pages/ClientRegistryPage/ClientRegistryPage.jsx
@@ -13,69 +13,64 @@ export default function ClientRegistryPage() {
   const [showPassword, setShowPassword] = React.useState(false);
 
   return (
-    <main className="main main-registry-client">
+    <>
       <RegistryTabs />
 
-      <HeaderTexts isRegistry={true} />
-
-      <form className="form">
-        <div className="form__input-group">
-          <div className="input-group">
-            <input required="required" type="text" id="form__name" />
-            <label htmlFor="form__name">Nombre</label>
-            <div className="input__icon">
-              <MdDescription />
+      <main className="main main-registry-client">
+        <HeaderTexts isRegistry={true} />
+        <form className="form">
+          <div className="form__input-group">
+            <div className="input-group">
+              <input required="required" type="text" id="form__name" />
+              <label htmlFor="form__name">Nombre</label>
+              <div className="input__icon">
+                <MdDescription />
+              </div>
+            </div>
+            <div className="input-group">
+              <input required="required" type="text" id="form__lastname" />
+              <label htmlFor="form__lastname">Apellidos</label>
+              <div className="input__icon">
+                <MdDescription />
+              </div>
             </div>
           </div>
-
           <div className="input-group">
-            <input required="required" type="text" id="form__lastname" />
-            <label htmlFor="form__lastname">Apellidos</label>
+            <input required="required" type="tel" id="form__phone" />
+            <label htmlFor="form__phone">Telefono</label>
             <div className="input__icon">
-              <MdDescription />
+              <FaPhoneAlt />
             </div>
           </div>
-        </div>
-
-        <div className="input-group">
-          <input required="required" type="tel" id="form__phone" />
-          <label htmlFor="form__phone">Telefono</label>
-          <div className="input__icon">
-            <FaPhoneAlt />
+          <div className="input-group">
+            <input required="required" type="text" id="form__email" />
+            <label htmlFor="form__email">Email</label>
+            <div className="input__icon">
+              <MdEmail />
+            </div>
           </div>
-        </div>
-
-        <div className="input-group">
-          <input required="required" type="text" id="form__email" />
-          <label htmlFor="form__email">Email</label>
-          <div className="input__icon">
-            <MdEmail />
+          <div className="input-group">
+            <input
+              required="required"
+              type={showPassword ? 'text' : 'password'}
+              id="form__password"
+            />
+            <label htmlFor="form__password">Contraseña</label>
+            <div
+              className="input__icon input__icon--password"
+              onClick={() => setShowPassword(!showPassword)}>
+              {showPassword ? <AiFillEyeInvisible /> : <AiFillEye />}
+            </div>
           </div>
-        </div>
-
-        <div className="input-group">
-          <input
-            required="required"
-            type={showPassword ? 'text' : 'password'}
-            id="form__password"
-          />
-          <label htmlFor="form__password">Contraseña</label>
-          <div
-            className="input__icon input__icon--password"
-            onClick={() => setShowPassword(!showPassword)}>
-            {showPassword ? <AiFillEyeInvisible /> : <AiFillEye />}
-          </div>
-        </div>
-
-        <button className="button">Crear cuenta</button>
-
-        <p>
-          Al registrarte, aceptas nuestros <a href="">Terminos y condiciones</a>{' '}
-          y la <a href="">Politica de Privacidad</a>
-        </p>
-      </form>
-
-      <SocialButtons />
-    </main>
+          <button className="button">Crear cuenta</button>
+          <p>
+            Al registrarte, aceptas nuestros{' '}
+            <a href="">Terminos y condiciones</a> y la{' '}
+            <a href="">Politica de Privacidad</a>
+          </p>
+        </form>
+        <SocialButtons />
+      </main>
+    </>
   );
 }
--- src/pages/EstablishmentRegistryPage/EstablishmentRegistryPage.jsx
+++ src/pages/EstablishmentRegistryPage/EstablishmentRegistryPage.jsx
@@ -15,97 +15,100 @@ export default function EstablishmentRegistryPage() {
   const [showPassword, setShowPassword] = React.useState(false);
 
   return (
-    <main className="main main-registry-client">
+    <>
       <RegistryTabs isClient={false} />
 
-      <HeaderTexts isRegistry={true} />
+      <main className="main main-registry-establishment">
+        <HeaderTexts isRegistry={true} />
 
-      <form className="form">
-        <div className="form__input-group">
+        <form className="form">
+          <div className="form__input-group">
+            <div className="input-group">
+              <input required="required" type="text" id="form__name" />
+              <label htmlFor="form__name">Nombre</label>
+              <div className="input__icon">
+                <MdDescription />
+              </div>
+            </div>
+
+            <div className="input-group">
+              <input required="required" type="text" id="form__lastname" />
+              <label htmlFor="form__lastname">Apellidos</label>
+              <div className="input__icon">
+                <MdDescription />
+              </div>
+            </div>
+          </div>
+
+          <div className="form__input-group">
+            <div className="input-group">
+              <input required="required" type="tel" id="form__phone" />
+              <label htmlFor="form__phone">Telefono</label>
+              <div className="input__icon">
+                <FaPhoneAlt />
+              </div>
+            </div>
+
+            <div className="input-group">
+              <input required="required" type="text" id="form__lastname" />
+              <label htmlFor="form__lastname">Dirección</label>
+              <div className="input__icon">
+                <TbBuildingEstate />
+              </div>
+            </div>
+          </div>
+
+          <div className="form__input-group">
+            <div className="input-group">
+              <input required="required" type="text" id="form__name" />
+              <label htmlFor="form__name">Ciudad</label>
+              <div className="input__icon">
+                <MdLocationCity />
+              </div>
+            </div>
+
+            <div className="input-group">
+              <input required="required" type="text" id="form__province" />
+              <label htmlFor="form__province">Provincia</label>
+              <div className="input__icon input__icon--special">
+                <GrMapLocation />
+              </div>
+            </div>
+          </div>
+
           <div className="input-group">
-            <input required="required" type="text" id="form__name" />
-            <label htmlFor="form__name">Nombre</label>
+            <input required="required" type="text" id="form__email" />
+            <label htmlFor="form__email">Provincia</label>
             <div className="input__icon">
-              <MdDescription />
+              <MdEmail />
             </div>
           </div>
 
           <div className="input-group">
-            <input required="required" type="text" id="form__lastname" />
-            <label htmlFor="form__lastname">Apellidos</label>
-            <div className="input__icon">
-              <MdDescription />
-            </div>
-          </div>
-        </div>
-
-        <div className="form__input-group">
-          <div className="input-group">
-            <input required="required" type="tel" id="form__phone" />
-            <label htmlFor="form__phone">Telefono</label>
-            <div className="input__icon">
-              <FaPhoneAlt />
+            <input
+              required="required"
+              type={showPassword ? 'text' : 'password'}
+              id="form__password"
+            />
+            <label htmlFor="form__password">Contraseña</label>
+            <div
+              className="input__icon input__icon--password"
+              onClick={() => setShowPassword(!showPassword)}>
+              {showPassword ? <AiFillEyeInvisible /> : <AiFillEye />}
             </div>
           </div>
 
-          <div className="input-group">
-            <input required="required" type="text" id="form__lastname" />
-            <label htmlFor="form__lastname">Dirección</label>
-            <div className="input__icon">
-              <TbBuildingEstate />
-            </div>
-          </div>
-        </div>
+          <button className="button">Crear cuenta</button>
 
-        <div className="form__input-group">
-          <div className="input-group">
-            <input required="required" type="text" id="form__name" />
-            <label htmlFor="form__name">Ciudad</label>
-            <div className="input__icon">
-              <MdLocationCity />
-            </div>
-          </div>
+          <p>
+            Al registrarte, aceptas nuestros{' '}
+            <a href="">Terminos y condiciones</a> y la{' '}
+            <a href="">Politica de Privacidad</a>
+          </p>
+        </form>
 
-          <div className="input-group">
-            <input required="required" type="text" id="form__province" />
-            <label htmlFor="form__province">Provincia</label>
-            <div className="input__icon input__icon--special">
-              <GrMapLocation />
-            </div>
-          </div>
-        </div>
-
-        <div className="input-group">
-          <input required="required" type="text" id="form__email" />
-          <label htmlFor="form__email">Provincia</label>
-          <div className="input__icon">
-            <MdEmail />
-          </div>
-        </div>
-
-        <div className="input-group">
-          <input
-            required="required"
-            type={showPassword ? 'text' : 'password'}
-            id="form__password"
-          />
-          <label htmlFor="form__password">Contraseña</label>
-          <div
-            className="input__icon input__icon--password"
-            onClick={() => setShowPassword(!showPassword)}>
-            {showPassword ? <AiFillEyeInvisible /> : <AiFillEye />}
-          </div>
-        </div>
-
-        <button className="button">Crear cuenta</button>
-
-        <p>
-          Al registrarte, aceptas nuestros <a href="">Terminos y condiciones</a>{' '}
-          y la <a href="">Politica de Privacidad</a>
-        </p>
-      </form>
-
-      <SocialButtons />
-    </main>
+        <SocialButtons />
+      </main>
+    </>
   );
 }
